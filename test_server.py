e134f81a2687e56bd0b801b7a1d5d8ac78086eb5
onepinetree <148179315+onepinetree@users.noreply.github.com>
2024-08-02 01:24:22 +0900

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -8,24 +8,24 @@ app = FastAPI()
 
 class Model(BaseModel):
     climax: int 
-    time_back: int  
+    time_back: str
     url: str
 
 
-@app.get("/modified_link")
+@app.post("/modified_link")
 def modify_link(model: Model):
 
     base_link: str = model.url
     base_link = unquote(base_link)
 
-    request_start_time : int = model.climax - model.time_back
+    request_start_time : int = model.climax - int(model.time_back)
     
     if 't=' in base_link:
         base_link = base_link.split('t=')[0]
 
     modified_link = f'{base_link}&t={request_start_time}s'
 
-    return {"modified_url": modified_link}
+    return modified_link
 
 #로컬환경 설정
 # if __name__ == "__main__":
